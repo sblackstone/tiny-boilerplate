fa443a80cd03487f22015198fafd0d9e9fa703ca
Stephen Blackstone <sblackstone@gmail.com>
2015-07-21 12:00:52 -0400

    Remove jquery, this boilerplate is all CDN

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 requirejs.config({
   baseUrl: 'js',
   paths: {
-	  jquery: 'vendors/jquery-2.1.4.min',
+	  jquery: '//code.jquery.com/jquery-2.1.4.min',
     bootstrap:  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min" 
   }, 
   shim: {
